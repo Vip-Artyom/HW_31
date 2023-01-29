2ea4364e62fc4edf5fd43dbae02f4689889a2fa3
Vip-Artyom <snowboard-team@mail.ru>
2023-01-29 17:09:05 +0600

    step 3

--- ads/views/ad.py
+++ ads/views/ad.py
@@ -3,6 +3,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import UpdateView
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ads.serializers.ad_serializers import *
 
@@ -18,10 +19,17 @@ class AdViewSet(ModelViewSet):
         'retrieve': AdDetailSerializer,
         'list': AdListSerializer,
     }
+
+    default_permissions = [AllowAny()]
+    permission_list = {"retrieve": [IsAuthenticated()]}
+
     pagination_class = AdPagination
 
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, self.default_serializer)
+
+    def get_permissions(self):
+        return self.permission_list.get(self.action, self.default_permissions)
 
     def list(self, request, *args, **kwargs):
 
--- shop/settings.py
+++ shop/settings.py
@@ -134,6 +134,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 3,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 AUTH_USER_MODEL = 'users.Users'
